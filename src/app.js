a4007b6fd212256faedf76dad5e3520eb5d2446b
santimedina <sjmedi.97@gmail.com>
2022-07-23 19:23:19 -0300

    commit 2

--- src/app.js
+++ src/app.js
@@ -5,5 +5,7 @@ app.get('/', (req, res) => {
     res.send('Bienvenido a la pagina principal!!!!')
 });
 
+app.use(express.static('public'));
+
 
 app.listen(3000, () => console.log('Proyecto en el puerto 3000'));
