3d7f2a8eca33bd55f1d17713be84ce6fcc49b0da
santimedina <sjmedi.97@gmail.com>
2022-07-23 20:16:14 -0300

    commit 3

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
 
+//Registra ejs como motor de plantillas
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/views'));
+
+
 app.get('/', (req, res) => {
     res.send('Bienvenido a la pagina principal!!!!')
 });
 
+//Registra como publica/static la carpeta public
 app.use(express.static('public'));
 
+// Registra carpetas estaticas con "prefijos"
+app.use('/prefijo', express.static('publica'));
 
-app.listen(3000, () => console.log('Proyecto en el puerto 3000'));
+
+app.get('/index', (req, res) => {
+    res.render('index');
+})
+
+app.listen(3000, () => console.log('Proyecto en el puerto 3000')); 
